c83ae1ab1ec25931c271784fbde60d81e424e532
jkmd96 <joshlinnas@gmail.com>
2023-12-25 11:52:34 +0300

    update: change toast position

--- src/components/common/notifications/ToastComponent.tsx
+++ src/components/common/notifications/ToastComponent.tsx
@@ -5,6 +5,6 @@ import "react-toastify/dist/ReactToastify.css";
 
 export default function ToastContainerWrapper() {
     return (
-        <ToastContainer position={toast.POSITION.BOTTOM_RIGHT}/>
+        <ToastContainer position={toast.POSITION.TOP_RIGHT}/>
     );
 }
