f9f994118e5fe59d0193bff44af778804b444bc4
jkmd96 <joshlinnas@gmail.com>
2024-01-06 00:24:08 +0300

    feat: remove write to file and add pino on api routes

--- src/app/api/journal-entry/list/route.ts
+++ src/app/api/journal-entry/list/route.ts
@@ -2,25 +2,19 @@ import {handleApiException, handleAxiosResponse} from "@/helpers/responseHelpers
 import petJournalApiClient, {getAxiosConfigs} from "@/lib/axios/axiosClient";
 import {NextRequest} from "next/server";
 import {getJournalQueryParams} from "@/helpers/urlHelpers";
-import fs from "fs";
-import path from "path";
 
 export async function GET(request: NextRequest) {
     try {
-        const rootDirectory = process.cwd();
-        const filePath = path.join(rootDirectory, 'log-file.txt');
-
         const queryParams = getJournalQueryParams(request);
         const config = getAxiosConfigs(request, queryParams);
         const response = await petJournalApiClient.get('journal-entry', config);
+        const onlyPino = require('pino')()
 
-        fs.appendFile(filePath, JSON.stringify(response.data) + '\n', (err) => {
-            if (err) {
-                console.error('Error writing to the file:', err);
-            }
-        });
+        onlyPino.info(response,"fetch journal entries response")
         return handleAxiosResponse(response);
     } catch (error: unknown) {
+        const onlyPino = require('pino')()
+        onlyPino.error(error,"An error occurred fetching journal entries")
         return handleApiException(error);
     }
 }
