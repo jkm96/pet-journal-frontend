87c1bdf356acf6cedc257374e4c037455d8dc485
jkmd96 <joshlinnas@gmail.com>
2024-01-06 00:51:34 +0300

    feat: add logflare for logging

--- src/app/api/journal-entry/list/route.ts
+++ src/app/api/journal-entry/list/route.ts
@@ -2,19 +2,18 @@ import {handleApiException, handleAxiosResponse} from "@/helpers/responseHelpers
 import petJournalApiClient, {getAxiosConfigs} from "@/lib/axios/axiosClient";
 import {NextRequest} from "next/server";
 import {getJournalQueryParams} from "@/helpers/urlHelpers";
+import logger from "@/logger/logger";
 
 export async function GET(request: NextRequest) {
     try {
-        const onlyPino = require('pino')()
         const queryParams = getJournalQueryParams(request);
-        onlyPino.info(queryParams,"query params")
+        logger.info(queryParams,"query params")
         const config = getAxiosConfigs(request, queryParams);
         const response = await petJournalApiClient.get('journal-entry', config);
-        onlyPino.info(response,"fetch journal entries response")
+        logger.info(response,"fetch journal entries response")
         return handleAxiosResponse(response);
     } catch (error: unknown) {
-        const onlyPino = require('pino')()
-        onlyPino.error(error,"An error occurred fetching journal entries")
+        logger.error(error,"An error occurred fetching journal entries")
         return handleApiException(error);
     }
 }
--- src/app/api/pet/profile/route.ts
+++ src/app/api/pet/profile/route.ts
@@ -1,13 +1,13 @@
 import {handleApiException, handleAxiosResponse} from "@/helpers/responseHelpers";
 import petJournalApiClient, {getAxiosConfigs} from "@/lib/axios/axiosClient";
 import {NextRequest} from "next/server";
+import logger from "@/logger/logger";
 
 export async function POST(request: NextRequest) {
     try {
-        const onlyPino = require('pino')()
         const config = getAxiosConfigs(request);
         const response = await petJournalApiClient.get('pet/profiles', config);
-        onlyPino.info(response,"pet profiles response")
+        logger.info(response,"pet profiles response")
         return handleAxiosResponse(response);
     } catch (error: unknown) {
         return handleApiException(error);
