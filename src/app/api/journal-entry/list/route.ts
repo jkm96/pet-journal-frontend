8be3a7fdce1f0247664e6d8aaa114950b71e834c
jkmd96 <joshlinnas@gmail.com>
2024-01-06 12:58:25 +0300

    fix: vercel deployment

--- src/app/api/journal-entry/list/route.ts
+++ src/app/api/journal-entry/list/route.ts
@@ -5,13 +5,12 @@ import {getJournalQueryParams} from "@/helpers/urlHelpers";
 import logger from "@/logger/logger";
 export async function POST(request: NextRequest) {
     try {
-        logger.info(`reached api endpoint`)
         logger.info(`reached api endpoint with ${JSON.stringify(request.nextUrl)}`)
         const queryParams = getJournalQueryParams(await request.json());
         logger.error(`reached api endpoint with ${JSON.stringify(queryParams)}`)
         const config = getAxiosConfigs(request, queryParams);
         const response = await petJournalApiClient.get('journal-entry', config);
-        // logger.info(response,"fetch journal entries response")
+        logger.info(response,"fetch journal entries response")
         return handleAxiosResponse(response);
     } catch (error: unknown) {
         logger.error(error,"An error occurred fetching journal entries")
