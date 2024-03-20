f5faeb9a8c917af83ac25f6f24fe7163ee228b01
jkmdev <jkm96.dev@gmail.com>
2024-03-20 21:09:05 +0300

    hotfix: grace period count fix

--- src/components/common/notifications/UnverifiedEmailMessage.tsx
+++ src/components/common/notifications/UnverifiedEmailMessage.tsx
@@ -7,7 +7,6 @@ export default function UnverifiedEmailMessage() {
   const [userInfo, setUserInfo] = useState({} as User);
 
   useEffect(() => {
-    console.log("unverified email component")
     if (user) {
       setUserInfo(user);
     }
@@ -15,25 +14,33 @@ export default function UnverifiedEmailMessage() {
 
   return (
     <>
-      {authLoading ? (<></>): (
+      {authLoading ? (<></>) : (
         <>
-          {userInfo && !userInfo.isAdmin && (
-            <div
-              className='w-full grid p-2 place-items-center bg-yellow-100 dark:bg-boxdark dark:border-1 dark:border-gray'>
-              <div
-                className='flex items-center text-sm text-black-2 dark:text-yellow-400' role='alert'>
-                <svg className='flex-shrink-0 inline w-4 h-4 me-3' aria-hidden='true' xmlns='http://www.w3.org/2000/svg'
-                     fill='currentColor' viewBox='0 0 20 20'>
-                  <path
-                    d='M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z' />
-                </svg>
-                <span className='sr-only'>Info</span>
-                <div>
-                  <span className='font-medium'>Email Verification Alert!</span> You have {userInfo.gracePeriodCount} days
-                  to verify your email to continue accessing this resource.
+          {userInfo && userInfo.isEmailVerified ? (
+            <></>
+          ) : (
+            <>
+              {!userInfo.isAdmin && (
+                <div
+                  className='w-full grid p-2 place-items-center bg-yellow-100 dark:bg-boxdark dark:border-1 dark:border-gray'>
+                  <div
+                    className='flex items-center text-sm text-black-2 dark:text-yellow-400' role='alert'>
+                    <svg className='flex-shrink-0 inline w-4 h-4 me-3' aria-hidden='true'
+                         xmlns='http://www.w3.org/2000/svg'
+                         fill='currentColor' viewBox='0 0 20 20'>
+                      <path
+                        d='M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z' />
+                    </svg>
+                    <span className='sr-only'>Info</span>
+                    <div>
+                      <span className='font-medium'>Email Verification Alert!</span> You
+                      have {userInfo.gracePeriodCount} days
+                      to verify your email to continue accessing this resource.
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
+              )}
+            </>
           )}
         </>
       )}
