d6531ad0fde886e5f4a940d2f7fc1fdf9dd81e92
jkmdev <jkm96.dev@gmail.com>
2024-04-10 19:37:20 +0300

    fix: test seo on srr

--- src/app/features/page.tsx
+++ src/app/features/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 import React, { useEffect, useState } from 'react';
 import Loader from '@/components/common/dashboard/Loader';
 import { CustomerFeedback } from '@/components/site/CustomerFeedback';
@@ -7,24 +6,25 @@ import { Benefits } from '@/components/site/sections/Benefits';
 import Features from '@/components/site/Features';
 
 export default function FeaturesPage() {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setLoading(false);
-    }, 1000); //1 second
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     setLoading(false);
+  //   }, 1000); //1 second
+  //
+  //   return () => clearTimeout(timeout);
+  // }, []);
+  //
+  // const title = 'Features - Online Diary for Pet Lovers. Keep your pet memories together';
+  // const description = 'Pet Diaries helps you save your daily memories, download them at any time, and print them beautifully.';
+  // const type = "website"
+  //
+  // HtmlPageHead(title,description,type);
 
-    return () => clearTimeout(timeout);
-  }, []);
-
-  const title = 'Features - Online Diary for Pet Lovers. Keep your pet memories together';
-  const description = 'Pet Diaries helps you save your daily memories, download them at any time, and print them beautifully.';
-  const type = "website"
-
-  HtmlPageHead(title,description,type);
-
-  if (loading) {
-    return <Loader />;
-  } else {
-    return <Features />;
-  }
+  // if (loading) {
+  //   return <Loader />;
+  // } else {
+  //   return <Features />;
+  // }
+  return <Features />;
 }
