897161ac5e793d52b671fa5aaf8717fa88060ec3
jkmdev <jkm96.dev@gmail.com>
2024-04-10 23:20:17 +0300

    fix: make reset user password page to be ssr

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,15 +1,18 @@
-'use client';
 import React from 'react';
 import ChangePassword from '@/components/auth/user/ChangePassword';
-import { HtmlPageHead } from '@/lib/utils/seoUtils';
+import { getMetadata } from '@/lib/utils/seoUtils';
+import { Metadata } from 'next';
+
+const title = 'Reset Password - Online Diary for Pet Lovers. Keep your pet memories together';
+const description = 'Pet Diaries helps you save your daily memories, download them at any time, and print them beautifully.';
+
+export async function generateMetadata(): Promise<Metadata> {
+  const currentUrl = typeof window !== 'undefined' ? window.location.href : '';
+  console.log('currentUrl', currentUrl);
+
+  return getMetadata(title, description);
+}
 
 export default function ChangePasswordPage() {
-
-  const title = 'Reset Password - Online Diary for Pet Lovers. Keep your pet memories together';
-  const description = 'Pet Diaries helps you save your daily memories, download them at any time, and print them beautifully.';
-  const type = "website"
-
-  HtmlPageHead(title,description,type);
-
   return <ChangePassword />;
 };
